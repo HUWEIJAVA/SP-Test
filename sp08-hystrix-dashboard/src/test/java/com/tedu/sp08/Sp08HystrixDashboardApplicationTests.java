d6722916611d59d2f3a430d6f56a96872cfab931
HUWEIJAVA <904993720@qq.com>
2019-11-01 14:50:50 +0800

    hystrix+dashboard仪表盘

---
+++ sp08-hystrix-dashboard/src/test/java/com/tedu/sp08/Sp08HystrixDashboardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp08;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp08HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
